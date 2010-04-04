633a95f88e47e77295468a3d7b6b95fd57c7e3a8
Robert Shaw <robert@or.homeip.net>
2010-04-04 15:31:42 -0700

    clamav: add PowerPC patches from ClamAV bug report

--- Library/Formula/clamav.rb
+++ Library/Formula/clamav.rb
@@ -5,6 +5,12 @@ class Clamav <Formula
   homepage 'http://www.clamav.net/'
   md5 '202e51d47298779e5babacc443102c6a'
 
+  def patches
+    # Patches to fix runtime abort bug in clamd on PowerPC.
+    # See: https://wwws.clamav.net/bugzilla/show_bug.cgi?id=1921
+    "https://wwws.clamav.net/bugzilla/attachment.cgi?id=1243"
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
     system "make install"
