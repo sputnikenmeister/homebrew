60868a3a8675a97357d4ba75d5fe58b8168347e8
Robert Shaw <robert914@me.com>
2011-05-11 12:44:44 -0700

    Updated: lmutil to v11.9.1

--- Library/Formula/lmutil.rb
+++ Library/Formula/lmutil.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Lmutil < Formula
-  url 'http://www.globes.com/products/utilities/v11.8/lmutil_universal_mac10.tar.gz'
-  version '11.8'
+  url 'http://www.globes.com/products/utilities/v11.9.1/lmutil_universal_mac10.tar.gz'
+  version '11.9.1'
   homepage 'http://www.globes.com/support/fnp_utilities_download.htm'
-  md5 '9754ab4101edf450de31454b6da0eec3'
+  md5 '0941f4f1b88e34ae2b50a463f88b05a7'
 
   # Stripping the binaries would invalidate the code signatures
   skip_clean ['bin']
